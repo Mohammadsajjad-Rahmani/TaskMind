e3f6f0985e779fdd72f8b3e3a45dd073a7d2d043
Mohammadsajjad <mohammadsajjadmail@gmail.com>
2026-09-21 20:53:41 +0330

    Add autheticate feature

---
+++ auth.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+import bcrypt
+from jose import jwt
+
+SECRET_KEY = "YOUR_SUPER_SECRET_KEY_CHANGE_IN_PRODUCTION"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # ۲۴ ساعت اعتبار
+
+def hash_password(password: str) -> str:
+    # تبدیل رشته به بایت و هش کردن
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # اعتبارسنجی پسورد ورودی با هش ذخیره شده
+    plain_bytes = plain_password.encode('utf-8')
+    hashed_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_bytes, hashed_bytes)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
