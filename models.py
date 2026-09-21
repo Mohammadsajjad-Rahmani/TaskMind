e2daf9503c7e4b8b56f5b2747cdbc854f95b9bc4
Mohammadsajjad <mohammadsajjadmail@gmail.com>
2026-09-21 20:56:58 +0330

    feat(models): add user_id foreign key and relationship to Task model

--- models.py
+++ models.py
@@ -1,6 +1,24 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+# اسکیمای ثبت‌نام
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+# اسکیمای خروجی کاربر
+class UserResponse(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        from_attributes = True
+
+# اسکیمای خروجی توکن JWT
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 class TaskCreate(BaseModel):
     title: str
     description: str
@@ -15,6 +33,9 @@ class TaskResponse(BaseModel):
     is_duplicate: bool
     duplicate_warning: Optional[str] = None
     is_completed: bool = False
+
+    class Config:
+        from_attributes = True
 
 class DailySummaryResponse(BaseModel):
     total_tasks: int
