675c232d30b6beed41672894b23a55df93a1a116
Mohammadsajjad <mohammadsajjadmail@gmail.com>
2026-09-23 14:00:03 +0330

    feat(deps): Update Error handeling section

--- dependencies.py
+++ dependencies.py
@@ -8,17 +8,28 @@ import auth
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+from jose import JWTError, ExpiredSignatureError, jwt
+
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> UserDB:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="توکن نامعتبر است یا منقضی شده است.",
+        detail="توکن نامعتبر است.",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
     try:
         payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
+            
+    except ExpiredSignatureError:
+        # 💡 خروجی دقیق برای توکن‌های منقضی‌شده
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="توکن شما منقضی شده است. لطفاً دوباره لاگین کنید.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     except JWTError:
         raise credentials_exception
 
